f9ad7fc3a45ab53824af186b412c7f66a874f3da
NamanGarg12 <alienworld789@gmail.com>
2026-02-14 03:16:50 +0530

    Making Functions

--- src/helper.py
+++ src/helper.py
@@ -1,10 +1,40 @@
 # Adding utility tools
-
-import fitz #PyMuPDF
+import fitz # PyMuPDF
 import os
 from dotenv import load_dotenv
+from groq import Groq
+from apify_client import ApifyClient
 
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=GROQ_API_KEY)
+apify_client = ApifyClient(os.getenv("APIFY_API_KEY"))
 
+def ask_groq(prompt, max_tokens=500): # This function to get the information like summary, gaps and missings, future roadmap
+    """
+        Sends a prompt to the groq API and return the response.
+    Args:
+        prompt (str): The prompt to send to the Groq.
+        model (str): The model to use for the request.
+        temperature (float): The temperature for the response.
+    Returns:
+        str: The response from the Groq API.
+    """
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile", 
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.5,
+        max_completion_tokens=max_tokens 
+    )
+
+    return response.choices[0].message.content
+    
 
 def extract_text_from_pdf(uploaded_file):
     """
@@ -22,3 +52,4 @@ def extract_text_from_pdf(uploaded_file):
         text += page.get_text()
     return text
         
+
---
+++ src/jobs_api.py
@@ -0,0 +1,37 @@
+from apify_client import ApifyClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apify_client = ApifyClient(os.getenv("APIFY_API_KEY"))
+
+#Function to get info about the open jobs from the Apify API Key 
+
+#Fetch Linkdin Jobs based on search query and the location
+def fetch_linkdin_jobs(search_query, location = "india", row = 60): # rows is the number of jobs you want to see 
+    run_input = {
+        "title" : search_query,
+        "location" : location,
+        "rows":row,
+        "proxy": {
+            "useApifyProxy": True,
+            "apifyProxyGroups": ["RESIDENTIAL"],
+        }
+    }
+    run = apify_client.actor("BHzefUZlZRKWxkTck").call(run_input=run_input)
+    jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items()) #Returning list of Jobs
+    return jobs
+
+#Fetch Nauki Jobs 
+def fetch_naukri_jobs(search_query, location = "india", row = 60):
+    run_input = {
+        "keyword":search_query,
+        "maxJobs": 60,
+        "freshness": "all",
+        "sortBy": "relevance",
+        "experience": "all",
+    }
+    run = apify_client.actor("alpcnRV9YI9lYVPWk").call(run_input=run_input)
+    jobs = list(apify_client.dataset(run["defaultDatasetId"]).iterate_items())
+    return jobs
